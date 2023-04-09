98f0c54561b6233dfcf0d8d8b74c184241b507e9
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-04-09 15:59:55 +0300

    fff

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+import { Notify } from './Notification.styled';
+
+const Notification = ({ message }) => <Notify>{message}</Notify>;
+
+Notification.propTypes = {
+    message: PropTypes.string.isRequired,
+};
+
+export default Notification;
